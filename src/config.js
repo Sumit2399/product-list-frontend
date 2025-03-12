8300b1cb358723228d381d11d724f809c3a424d7
Sumit Dhyani <66456021+Sumit2399@users.noreply.github.com>
2025-03-12 14:09:02 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-const API_URL = "http://localhost:3000"; // Change this to your deployed backend URL
+const API_URL = "https://productlist-web-e8begzbhbtafcuaa.centralindia-01.azurewebsites.net"; 
 export default API_URL;
